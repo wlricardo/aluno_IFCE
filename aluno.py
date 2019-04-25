2ea80217cddf95dc69232fa6283f35617a6a9528
Willem de Lima Ricardo <wlricardo@gmail.com>
2019-04-24 23:26:17 -0900

    Criãção de algumas classes

--- aluno.py
+++ aluno.py
@@ -3,5 +3,21 @@ class Aluno:
         self.__codigo = codigo
         self.__nome = nome
         self.__nascimento = nascimento
-        self.__api = ap1
-        self.__ap2 = ap2
+        if 0.0 <= ap1 <= 10.0:
+            self.__ap1 = ap1
+        else:
+            self.__ap1 = 11.0
+        if 0.0 <= ap2 <= 10.0:
+            self.__ap1 = ap1
+        else:
+            self.__ap2 = 11.00
+            
+        def get_nascimento:
+            return self.__nascimento
+        
+        
+        def get_ap1:
+            return self.__ap1
+        
+        def get_ap2:
+            return self.__ap2
